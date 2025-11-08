31759fc56309fc68b011d9b145f1a87aee9787e4
EmilioRoma <emilioroma2212@gmail.com>
2025-11-08 14:18:47 -0600

    feat: agregar funcionalidad de inicio de sesión con notificaciones tipo toast y estado de carga
    - Se implementó el componente AvisoToastStack para mostrar notificaciones.
    - Se creó el componente Loading para indicar el estado de carga.
    - Se desarrolló el componente Login con validación de formularios y gestión de sesiones.
    - Se introdujo el hook useAvisos para gestionar notificaciones tipo toast.
    - Se creo la base de la API con FastAPI.
    - Se definieron interfaces de TypeScript para la carga útil del token y los datos de inicio de sesión.
    - Se configuró Vite con soporte para React y Tailwind CSS.
    - Se configuraron los archivos de configuración de TypeScript para los entornos de la aplicación y Node.

---
+++ api/app/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from app.routes import usuarios
+from dotenv import load_dotenv
+
+load_dotenv()
+app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(usuarios.router)
